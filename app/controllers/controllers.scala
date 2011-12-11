ccb2b8445a380f2cc6bfcab6f56d3803286e674e
Charles O'Farrell <charleso@charleso.org>
2011-12-11 10:52:37 +1000

    Added Create Snippete page

--- app/controllers/controllers.scala
+++ app/controllers/controllers.scala
@@ -8,6 +8,8 @@ import play.templates.Html$
 import play.utils.HTML
 import models.SnippetType
 import play.db.anorm.Pk
+import play.db.anorm.Id
+import play.data.validation.Validation
 
 object Application extends Controller {
 
@@ -16,6 +18,33 @@ object Application extends Controller {
   def index() = {
     val snippets = Snippet.all
     html.index("Snippets", snippets)
+  }
+
+  def createSnippet() = {
+    html.create()
+  }
+
+  def create() = {
+    def get(s:String) = {
+        val value = params.get(s)
+        Validation.required(s, value)
+        value
+    }
+    val title = get("title")
+    val desc = get("desc")
+    val code = get("code")
+    val result = get("result")
+    if (Validation.hasErrors) {
+        Action(createSnippet())
+    } else {
+        val snippet = Snippet.create(new Snippet(NotAssigned, title, desc, code, result, false, 0, 1))
+        if (snippet.e.isLeft) {
+            flash += ("error" -> snippet.e.left.get.toString)
+            Action(index)
+        } else {
+            html.byType(List(snippet.e.right.get))
+        }
+    }
   }
 
   def topBar() = {
